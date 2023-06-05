ad1d21d01a4d9e807bf3048f1df16bdeed16f07f
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-06-05 21:37:00 +0700

    feat: home menu section

--- components/home/Home.vue
+++ components/home/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="p-4">
     <HomeBanner/>
+    <HomeMenuSection/>
   </div>
 </template>
--- components/home/HomeBanner.vue
+++ components/home/HomeBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-[url('/images/home-banner-bg.svg')] bg-cover bg-no-repeat h-[155px] w-full flex flex-row justify-between p-3 rounded-2xl overflow-hidden">
+  <div class="bg-[url('/images/home-banner-bg.svg')] bg-cover bg-no-repeat h-[155px] w-full flex flex-row justify-between p-3 mb-10 rounded-2xl overflow-hidden">
     <div class="flex-1 flex items-center max-w-[160px] sm:max-w-[280px]">
       <p class="text-white text-[11px] mobile-m:text-[14px] font-medium leading-4 mobile-m:leading-5">Bermain Badminton jadi makin mudah, asyik dan praktis. Mulai dengan Cari Lapangan.</p>
     </div>
