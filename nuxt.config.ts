1cdd08b6d47401f7ca1ffd8e1cfcb15a86321ada
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-06-05 19:31:07 +0700

    chore: install and configure tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   devServer: {
     port: 4001
-  }
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
