acc5cb47739d65816aafee6934510cf181e0337c
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-06-06 14:40:32 +0700

    feat: Test Deployment 6 Jun 2023 14.40 GMT+7

--- components/home/Home.vue
+++ components/home/Home.vue
@@ -2,6 +2,6 @@
   <div class="p-4">
     <HomeBanner/>
     <HomeMenuSection/>
-    <p>Test Deployment 6 Jun 2023 14.35 GMT+7</p>
+    <p>Test Deployment 6 Jun 2023 14.40 GMT+7</p>
   </div>
 </template>
