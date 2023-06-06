abf4e01ab3b741c54bc9de7e0047fd1e284cea59
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-06-06 14:37:34 +0700

    fix: enable ssr

--- components/home/Home.vue
+++ components/home/Home.vue
@@ -2,6 +2,6 @@
   <div class="p-4">
     <HomeBanner/>
     <HomeMenuSection/>
-    <p>Test Deployment 6 Jun 2023 13.39 GMT+7</p>
+    <p>Test Deployment 6 Jun 2023 14.35 GMT+7</p>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  routeRules: {
-    '/': { ssr: false },
-  },
   devServer: {
     port: 4001
   },
