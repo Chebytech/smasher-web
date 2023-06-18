74379caf548139af74b62448ff09fd0912d2063a
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-06-18 16:54:24 +0700

    fix: header title is not centered

--- components/header/HeaderBasic.vue
+++ components/header/HeaderBasic.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ArrowBackIcon from '~/assets/icons/arrow-back.svg'
-defineProps<{
+const props = defineProps<{
   title: string
+  onBack?: () => void
 }>()
 const router = useRouter()
+const handleBack = () => (props.onBack ? props.onBack() : router.back())
 </script>
 
 <template>
@@ -11,7 +13,7 @@ const router = useRouter()
     class="fixed top-0 right-0 left-0 bg-white h-[50px] sm:max-w-[576px] sm:mx-auto"
   >
     <nav class="flex items-center h-full">
-      <button class="mx-2" @click="router.push('/')">
+      <button class="mx-2 absolute" @click="handleBack()">
         <ArrowBackIcon filled class="!w-[32px] !h-[32px]" />
       </button>
       <div class="flex-1 text-center text-[18px] font-medium">{{ title }}</div>
