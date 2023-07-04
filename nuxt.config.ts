b2676cc7508f6afb83423df447e46f74a0cdd4ed
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-07-04 11:13:04 +0700

    feat: initialize PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', href: '/favicon.ico' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+      meta: [{ name: 'theme-color', content: '#ffffff' }],
+    },
+  },
   devServer: {
     port: 4001,
   },
   nitro: {
-    preset: 'vercel-edge',
+    preset: process.env.NITRO_PRESET,
+    prerender: {
+      routes: ['/', '/search/court'],
+    },
   },
   components: [
     {
@@ -23,5 +35,49 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true,
   },
-  modules: ['nuxt-svgo', '@hebilicious/vue-query-nuxt'],
+  modules: ['nuxt-svgo', '@hebilicious/vue-query-nuxt', '@vite-pwa/nuxt'],
+  pwa: {
+    disable: process.env.DISABLE_PWA === 'true',
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Smasher',
+      short_name: 'Smasher',
+      description: 'Smasher',
+      start_url: '/',
+      icons: [
+        {
+          src: '/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      lang: 'id',
+      scope: '/',
+    },
+    manifestFilename: 'site.webmanifest',
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      // periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  },
 })
