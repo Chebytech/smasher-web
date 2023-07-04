a2cfd5728b311935b0a504a7cd6bda9523907402
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-07-04 14:00:35 +0700

    feat: add recommended manifest properties

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,18 +49,32 @@ export default defineNuxtConfig({
           src: '/android-chrome-192x192.png',
           sizes: '192x192',
           type: 'image/png',
+          purpose: 'any',
         },
         {
           src: '/android-chrome-512x512.png',
           sizes: '512x512',
           type: 'image/png',
+          purpose: 'maskable',
         },
       ],
       theme_color: '#ffffff',
       background_color: '#ffffff',
       display: 'standalone',
+      display_override: ['window-controls-overlay', 'standalone', 'browser'],
       lang: 'id',
       scope: '/',
+      orientation: 'portrait',
+      categories: ['sports'],
+      launch_handler: {
+        client_mode: 'navigate-existing',
+      },
+      id: '/?homescreen=1',
+      dir: 'ltr',
+      edge_side_panel: {
+        preferred_width: 400,
+      },
+      prefer_related_applications: false,
     },
     manifestFilename: 'site.webmanifest',
     workbox: {
@@ -71,7 +85,7 @@ export default defineNuxtConfig({
       installPrompt: true,
       // you don't need to include this: only for testing purposes
       // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
-      // periodicSyncForUpdates: 20,
+      periodicSyncForUpdates: 3600 * 24,
     },
     devOptions: {
       enabled: true,
